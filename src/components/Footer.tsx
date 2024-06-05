e7788a07745c93a5818629a7bb5b2eefa402c447
OsmarReis <osmarzero26@gmail.com>
2024-06-05 15:52:07 -0300

    Created Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return(
+    <footer className="w-screen h-[4.5rem] flex items-center justify-center bg-yellow-200">
+      <p className="text-sm"><span>&copy;</span> 2024. Developed by ReisDev. All rights reserved.</p>
+    </footer>
+  )
+}
