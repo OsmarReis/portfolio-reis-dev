bec8175e517eb47d2501e3aeb6ebb165e1753193
OsmarReis <osmarzero26@gmail.com>
2024-06-06 16:58:51 -0300

    Minor useEffect changes and added CSS styles on header, div.img and button elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,8 @@ export default function Header() {
   const [responsive, setResponsive] = useState<Boolean | undefined>(undefined);
 
   useEffect(() => {
-    console.log("Olá");
     const updateResponsive = () => {
-      setResponsive(window.innerWidth < 796 ? true : false)
+      setResponsive(window.innerWidth < 768 ? true : false)
     }
     updateResponsive()
     window.addEventListener('resize', updateResponsive)
@@ -21,12 +20,15 @@ export default function Header() {
   }, [])
 
   return (
-    <header className="w-screen h-min fixed top-0 left-0 bg-yellow-200 flex justify-between items-center px-4 py-2 md:w-80 md:h-screen md:flex md:flex-col md:justify-start md:p-10">
-      <div className="relative size-12 md:size-40 md:mt-2 md:mb-16">
+    <header className="w-screen h-min fixed z-50 top-0 left-0 bg-yellow-200 flex justify-between items-center px-4 py-2 md:w-72 md:h-screen md:flex md:flex-col md:justify-start md:p-10">
+      <div className="relative overflow-hidden size-12 md:size-40 md:mt-2 md:mb-16">
         <Image 
           alt="A photo of me!"
           src="/profile.png"
           fill={true}
+          sizes="(max-width: 768px) 50vw, (max-width: 1200px) 25vw, 5vw"
+          className="border-2 border-red-500 rounded-full border-opacity-30 md:border-4"
+          priority
         />
       </div>
       {responsive !== undefined ? (
@@ -35,7 +37,7 @@ export default function Header() {
     ) : (
       <>
       <Navbar className="w-full mb-8"/>
-      <button className=" px-20 py-3 border rounded-full border-red-500 text-red-500 font-bold hover:bg-red-500 hover:text-white hover:border-white">Hire Me</button>
+      <button className="px-16 py-3 border rounded-full border-red-500 text-red-500 font-bold hover:bg-red-500 hover:text-white hover:border-white">Hire Me</button>
       </>
     )
   ) : null}
